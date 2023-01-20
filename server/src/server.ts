e9ed9f0be9e9df1532bdf9081a894a6a5610cbd6
JoaoVitorAR <jvalves1998@outlook.com.br>
2023-01-20 14:03:15 -0300

    Intergração mobile com back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,6 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors'
-import { prisma } from './lib/prisma';
 import { appRoutes } from './routes';
-
 
 const app = Fastify()
 
@@ -12,6 +10,7 @@ app.register(appRoutes)
 
 app.listen({
     port: 3333,
+    host: '0.0.0.0'
 }).then(() => {
     console.log('HTTP Server running!')
 })
